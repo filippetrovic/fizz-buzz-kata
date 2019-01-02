14ed46db0fafe14cb62258289004ea48af543941
Filip Petrovic <fpetrovic92@gmail.com>
2019-01-02 17:55:15 +0100

    Add rules to list

--- src/main/java/software/crafting/serbia/FizzBuzz.java
+++ src/main/java/software/crafting/serbia/FizzBuzz.java
@@ -1,15 +1,22 @@
 package software.crafting.serbia;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FizzBuzz {
 
   private final Rule fizzRule;
   private final Rule buzzRule;
   private final Rule fizzBuzzRule;
 
+  private List<Rule> rules;
+
   public FizzBuzz() {
     fizzRule = new Rule(new FizzMatcher(), new FizzSupplier());
     buzzRule = new Rule(new BuzzMatcher(), new BuzzSupplier());
     fizzBuzzRule = new Rule(new FizzMatcher().and(new BuzzMatcher()), new FizzBuzzSupplier());
+
+    rules = Arrays.asList(fizzBuzzRule, fizzRule, buzzRule);
   }
 
 
