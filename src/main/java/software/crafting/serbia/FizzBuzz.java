a4d308f477f7460b3a95e8b0994c53ac8f13af85
Filip Petrovic <fpetrovic92@gmail.com>
2019-01-02 16:05:44 +0100

    Extract constant

--- src/main/java/software/crafting/serbia/FizzBuzz.java
+++ src/main/java/software/crafting/serbia/FizzBuzz.java
@@ -2,9 +2,11 @@ package software.crafting.serbia;
 
 public class FizzBuzz {
 
+  public static final String FIZZ = "Fizz";
+
   public String execute(int number) {
     if (number % 3 == 0) {
-      return "Fizz";
+      return FIZZ;
     }
     return String.valueOf(number);
   }
