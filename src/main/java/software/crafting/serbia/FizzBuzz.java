6b459fc461b91e09bafccd2d4bd73576f12a60c9
Filip Petrovic <fpetrovic92@gmail.com>
2019-01-02 17:51:05 +0100

    Remove unsused methods

--- src/main/java/software/crafting/serbia/FizzBuzz.java
+++ src/main/java/software/crafting/serbia/FizzBuzz.java
@@ -24,19 +24,8 @@ public class FizzBuzz {
     return unchanged(number);
   }
 
-  private boolean isFizzBuzz(int number) {
-    return isFizz(number) && isBuzz(number);
-  }
-
-  private boolean isBuzz(int number) {
-    return number % 5 == 0;
-  }
-
   private String unchanged(int number) {
     return String.valueOf(number);
   }
 
-  private boolean isFizz(int number) {
-    return number % 3 == 0;
-  }
 }
