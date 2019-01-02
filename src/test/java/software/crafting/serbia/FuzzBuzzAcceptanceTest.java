7a037433138403c070381e3667368f7433b19447
Filip Petrovic <fpetrovic92@gmail.com>
2019-01-02 16:00:36 +0100

    Add more test cases for simple scenario

--- src/test/java/software/crafting/serbia/FuzzBuzzAcceptanceTest.java
+++ src/test/java/software/crafting/serbia/FuzzBuzzAcceptanceTest.java
@@ -11,5 +11,13 @@ public class FuzzBuzzAcceptanceTest {
   @Test
   public void shouldReturnUnchangedNumber() {
     assertThat(fizzBuzz.execute(1)).isEqualTo("1");
+
+    assertThat(fizzBuzz.execute(4)).isEqualTo("4");
+
+    assertThat(fizzBuzz.execute(22)).isEqualTo("22");
+
+    assertThat(fizzBuzz.execute(16)).isEqualTo("16");
+
+    assertThat(fizzBuzz.execute(98)).isEqualTo("98");
   }
 }
