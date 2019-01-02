0a9cf481dd83ba5d2dad09578a3d457bb13e75b3
Filip Petrovic <fpetrovic92@gmail.com>
2019-01-02 16:25:47 +0100

    Return FizzBuzz if number is Fizz and Buzz

--- src/main/java/software/crafting/serbia/FizzBuzz.java
+++ src/main/java/software/crafting/serbia/FizzBuzz.java
@@ -6,7 +6,7 @@ public class FizzBuzz {
   public static final String BUZZ = "Buzz";
 
   public String execute(int number) {
-    if (number % 15 == 0) {
+    if (isFizz(number) && isBuzz(number)) {
       return "FizzBuzz";
     }
 
