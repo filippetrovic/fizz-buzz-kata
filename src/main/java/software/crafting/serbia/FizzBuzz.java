b0609cd74f1e41e787055ee9a836d3411cb46b7f
Filip Petrovic <fpetrovic92@gmail.com>
2019-01-02 17:49:23 +0100

    Introduce FizzBuzzRule and use it as matcher

--- src/main/java/software/crafting/serbia/FizzBuzz.java
+++ src/main/java/software/crafting/serbia/FizzBuzz.java
@@ -7,10 +7,11 @@ public class FizzBuzz {
 
   private final Rule fizzRule = new Rule(new FizzMatcher(), new FizzSupplier());
   private final Rule buzzRule = new Rule(new BuzzMatcher(), new BuzzSupplier());
+  private final Rule fizzBuzzRule = new Rule(new FizzMatcher().and(new BuzzMatcher()), new BuzzSupplier());
 
 
   public String execute(int number) {
-    if (isFizzBuzz(number)) {
+    if (fizzBuzzRule.matches(number)) {
       return FIZZ_BUZZ;
     }
 
