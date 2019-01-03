86a596749dfa1753eb40e6061e066a32019a5623
Filip Petrovic <fpetrovic92@gmail.com>
2019-01-03 17:03:25 +0100

    Inject RuleChain in FizzBuzz

--- src/main/java/software/crafting/serbia/FizzBuzz.java
+++ src/main/java/software/crafting/serbia/FizzBuzz.java
@@ -1,32 +1,13 @@
 package software.crafting.serbia;
 
-import software.crafting.serbia.rule.Rule;
 import software.crafting.serbia.rule.RuleChain;
-import software.crafting.serbia.rule.RuleChainBuilder;
-import software.crafting.serbia.rule.predicate.IsNumberDivisibleByFive;
-import software.crafting.serbia.rule.predicate.IsNumberDivisibleByThree;
-import software.crafting.serbia.rule.predicate.MatchAll;
-import software.crafting.serbia.rule.transformer.BuzzTransformer;
-import software.crafting.serbia.rule.transformer.FizzBuzzTransformer;
-import software.crafting.serbia.rule.transformer.FizzTransformer;
-import software.crafting.serbia.rule.transformer.NoOppTransformer;
 
 public class FizzBuzz {
 
   private final RuleChain ruleChain;
 
-  public FizzBuzz() {
-    Rule fizzBuzzRule = new Rule(new IsNumberDivisibleByThree().and(new IsNumberDivisibleByFive()), new FizzBuzzTransformer());
-    Rule fizzRule = new Rule(new IsNumberDivisibleByThree(), new FizzTransformer());
-    Rule buzzRule = new Rule(new IsNumberDivisibleByFive(), new BuzzTransformer());
-    Rule otherRule = new Rule(new MatchAll(), new NoOppTransformer());
-
-    ruleChain = new RuleChainBuilder()
-        .addNextRule(fizzBuzzRule)
-        .addNextRule(fizzRule)
-        .addNextRule(buzzRule)
-        .addNextRule(otherRule)
-        .createRuleChain();
+  public FizzBuzz(RuleChain ruleChain) {
+    this.ruleChain = ruleChain;
   }
 
 
--- src/test/java/software/crafting/serbia/FuzzBuzzAcceptanceTest.java
+++ src/test/java/software/crafting/serbia/FuzzBuzzAcceptanceTest.java
@@ -1,12 +1,37 @@
 package software.crafting.serbia;
 
+import org.junit.Before;
 import org.junit.Test;
+import software.crafting.serbia.rule.Rule;
+import software.crafting.serbia.rule.RuleChainBuilder;
+import software.crafting.serbia.rule.predicate.IsNumberDivisibleByFive;
+import software.crafting.serbia.rule.predicate.IsNumberDivisibleByThree;
+import software.crafting.serbia.rule.predicate.MatchAll;
+import software.crafting.serbia.rule.transformer.BuzzTransformer;
+import software.crafting.serbia.rule.transformer.FizzBuzzTransformer;
+import software.crafting.serbia.rule.transformer.FizzTransformer;
+import software.crafting.serbia.rule.transformer.NoOppTransformer;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class FuzzBuzzAcceptanceTest {
 
-  private FizzBuzz fizzBuzz = new FizzBuzz();
+  private FizzBuzz fizzBuzz;
+
+  @Before
+  public void setup() {
+    Rule fizzBuzzRule = new Rule(new IsNumberDivisibleByThree().and(new IsNumberDivisibleByFive()), new FizzBuzzTransformer());
+    Rule fizzRule = new Rule(new IsNumberDivisibleByThree(), new FizzTransformer());
+    Rule buzzRule = new Rule(new IsNumberDivisibleByFive(), new BuzzTransformer());
+    Rule otherRule = new Rule(new MatchAll(), new NoOppTransformer());
+
+    fizzBuzz = new FizzBuzz(new RuleChainBuilder()
+        .addNextRule(fizzBuzzRule)
+        .addNextRule(fizzRule)
+        .addNextRule(buzzRule)
+        .addNextRule(otherRule)
+        .createRuleChain());
+  }
 
   @Test
   public void shouldReturnUnchangedNumberIfItsNotDivisibleBy3or5() {
